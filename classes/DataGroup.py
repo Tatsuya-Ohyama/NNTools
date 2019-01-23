8005ad80c3a6a2b9ffa0c467de60f22c26c79a35
mumeiyamibito <mumeiyamibito@gmail.com>
2019-01-23 10:35:05 +0900

    fix: replace list of data with pandas.DataFrame

--- classes/DataGroup.py
+++ classes/DataGroup.py
@@ -5,17 +5,15 @@ import sys
 import pickle
 import numpy as np
 import statistics
+import pandas as pd
 
 
 # =============== class =============== #
 class DataGroup:
-	""" LinearFunction class """
+	""" DataGroup class """
 	def __init__(self, data = None):
 		# member variables
-		self._datas = []
-		self._slope = 0.0
-		self._intercept = 0.0
-		self._r2 = 0.0
+		self._datas = pd.DataFrame()
 
 		# initiation
 		self.set_data(data)
@@ -49,19 +47,108 @@ class DataGroup:
 		@return: self
 		"""
 		if data is not None:
-			self._datas = data
+			self._datas = pd.DataFrame(data)
 		return self
 
 
-	def add_data(self, data):
+	def add_data(self, data, data_type = None):
 		"""
 		add data method
 		@param data: additional data
 		@return self
 		"""
 		if data is not None:
-			self._datas.append(data)
+			if data_type is None:
+				data_type = "object"
+			self._datas = self._datas.append(pd.Series(data, dtype = data_type), ignore_index = True)
 		return self
+
+
+	def set_label(self, label_type, label_list):
+		"""
+		set label for row or column
+		@param label_type: "row" or "column"
+		@param label_list: label list
+		@return self
+		"""
+		if label_type == "row":
+			self._datas.index = label_list
+		elif label_type == "column":
+			self._datas.columns = label_list
+		else:
+			sys.stderr.write("ERROR: undefined label_type at set_label() in DataGroup class.\n")
+			sys.exit(1)
+
+
+	def set_dtype(self, data_type, label_type = None, label_val = None):
+		"""
+		set dtype
+		@param data_type: data type
+		@param label_type: label value type for column ("label" or "index")
+		@param label_val: label value for changing type
+		@return self
+		"""
+		if label_type is not None:
+			if label_type == "label":
+				if label_val is not None:
+					self._datas.at[label_val].astype(data_type)
+				else:
+					sys.stderr.write("ERROR: label_val is not defined at set_dtype() in DataGroup class.\n")
+					sys.exit(1)
+			elif label_type == "index":
+				if label_val is not None:
+					self._datas[label_val].astype(data_type)
+				else:
+					sys.stderr.write("ERROR: label_val is not defined at set_dtype() in DataGroup class.\n")
+					sys.exit(1)
+			else:
+				sys.stderr.write("ERROR: undefined label_type at set_dtype() in DataGroup class.\n")
+				sys.exit(1)
+		else:
+			self._datas = self._datas.astype(data_type)
+		return self
+
+
+	def update_data(self, label_type, index, column, new_val):
+		"""
+		update value at specified x
+		@param label_type: "label" or "index"
+		@param index: row position
+		@param column: column position
+		@param new_val: value
+		@return self
+		"""
+		if labe_type == "label":
+			self._datas.at[index, column] = new_val
+		elif label_type == "index":
+			self._datas.iat[index, column] = new_val
+		else:
+			sys.stderr.write("ERROR: undefined label_type at update_data() in DataGroup class.\n")
+			sys.exit(1)
+		return self
+
+
+	def get_label(self, label_type):
+		"""
+		return label list
+		@param label_type: "row" or "column"
+		@return label list
+		"""
+		if label_type == "row":
+			return self._datas.index
+		elif label_type == "column":
+			return self._datas.columns
+		else:
+			sys.stderr.write("ERROR: undefined label_type at get_label() in DataGroup class.\n")
+			sys.exit(1)
+
+
+	def get_dtype(self):
+		"""
+		return dtype
+		@return dtype
+		"""
+		return self._datas.dtypes
 
 
 	def get_data(self):
@@ -78,11 +165,11 @@ class DataGroup:
 		@param deg: Degree of the fitting polynomial
 		@return self: [polynomial_coefficients, residuals, r2]
 		"""
-		x = np.array([x[0] for x in self._datas], dtype = np.float)
-		y = np.array([x[1] for x in self._datas], dtype = np.float)
-		coeff = np.polyfit(x, y, deg).tolist()
+		x = self._datas[self._datas.columns[0]]
+		y = self._datas[self._datas.columns[1]]
+		coeff = np.polyfit(x, y, deg)
 		r2 = np.corrcoef(x, y)[0,1]
-		return coeff + [r2]
+		return coeff.tolist() + [r2]
 
 
 # =============== main =============== #
