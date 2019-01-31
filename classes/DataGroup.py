a475f5703f687d4da463b8748d6ab6def5dd7b2c
Tatsuya Ohyama <t.ohyama.0109.02442@gmail.com>
2019-01-31 15:46:26 +0900

    feat: add DataGroup2 class (DataGroup class Ver. 2)

--- classes/DataGroup.py
+++ classes/DataGroup.py
@@ -266,6 +266,20 @@ class DataGroup:
 		return coeff.tolist() + [r1, r2]
 
 
+	def get_diff(self, label_type, label_x, label_y):
+		"""
+		return difference^2
+		@param label_type: "index" or "label"
+		@param label_x
+		@param label_y
+		@return difference^2 and sum of difference^2
+		"""
+		diff = (self._datas[label_x].values - self._datas[label_y].values) ** 2
+		return [diff, np.sum(diff)]
+
+
+
+
 	def save_csv(self, output_file):
 		"""
 		save to csv file
@@ -274,6 +288,112 @@ class DataGroup:
 		"""
 		self._datas.to_csv(output_file, header = True, index = True)
 		return self
+
+
+
+import pickle
+
+class DataGroup2:
+	""" DataGroup class (Ver. 2) """
+	def __init__(self, name):
+		"""
+		@param name: this object name
+		"""
+		# member variables
+		self._sequences = []
+		self._energy = []
+
+
+	def save_pickle(self, output_file):
+		"""
+		Pickle ファイルに保存するメソッド
+		@param output_file: 出力する pickle ファイルのパス
+		@return: 自身を返す (チェーンメソッドのため)"""
+		with open(output_file, "wb") as obj_output:
+			pickle.dump(self, obj_output)
+		return self
+
+
+	def restore_pickle(self, input_file):
+		"""
+		Pickle ファイルから復元するメソッド
+		@param input_file: pickle ファイルのパス
+		@return: 自身を返す (チェーンメソッドのため)
+		"""
+		with open(input_file, "rb") as obj_input:
+			self = pickle.load(obj_input)
+		return self
+
+
+	def append(self, obj_sequence, exp_value):
+		"""
+		append sequence object and experimental data
+		@param obj_sequence: Sequence object
+		@param exp_value: experimental value
+		@return: self
+		"""
+		self._sequences.append(obj_sequence)
+		self._energy.append(exp_value)
+		return self
+
+
+	def get_sequence(self, data_type = None):
+		"""
+		return Sequence object
+		@param data_type: None or "sequence"
+		@return sequence object list for None or sequence string list for "sequence"
+		"""
+		if data_type is None:
+			return self._sequences
+		elif data_type == "sequence":
+			return [x.get_sequence() for x in self._sequence]
+
+
+	def get_exp_value(self, flag_title = False):
+		"""
+		return experimental value list
+		@return experimental value list
+		"""
+		if flag_title:
+			return [[sequence.get_sequence(), energy] for sequence, energy in zip(self._sequence, self._energy)]
+		else:
+			return self._energy
+
+
+	def get_stat(self, obj_parameter, data_type = None, deg = 1):
+		"""
+		return statistics
+		@param data_type: None, "r", "r2", "slope", "intercept", "diff" (Default: None)
+		@param obj_parameter: degree of the fitting polynomial (Default: 1)
+		@param deg:
+		@return statistics value or return [r, r2, slope, intercept, diff_abs, diff_mean] list when data_type is None
+		"""
+		energy = [sequence.set_parameter(obj_parameter).get_energy() for sequence in self._sequence]
+		x = np.array(self._energy)
+		y = np.array(energy)
+		result = [float(x) for x in np.polyfit(x, y, deg).tolist]
+		result.append(np.correff(x, y)[0, 1])
+		result.append(result[-1] ** 2)
+		result.append(np.abs(x - y))
+		result.append(np.mean(x - y))
+
+		if data_type is None:
+			return result
+		elif data_type == "slope":
+			return result[0]
+		elif data_type == "intercept":
+			return result[1]
+		elif data_type == "r":
+			return result[2]
+		elif data_type == "r2":
+			return result[3]
+		elif data_type == "diff_abs":
+			return result[4]
+		elif data_type == "diff_mean":
+			return result[5]
+		else:
+			sys.stderr.write("ERROR: undefined data_type at get_stat() in DataGroup class.\n")
+			sys.exit(1)
 
 
 # =============== main =============== #
