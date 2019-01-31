da167909f5d96c1df0191a73524e0d362765290b
Tatsuya Ohyama <t.ohyama.0109.02442@gmail.com>
2019-01-31 14:35:52 +0900

    tmp

--- NNfitTool_iteration.py
+++ NNfitTool_iteration.py
@@ -47,15 +47,16 @@ if __name__ == '__main__':
 		# Ignore line number 1 (header) in CSV
 		next(reader)
 
-		for line_val in reader:
-			sequence = Sequence(line_val[2], line_val[0])
+		for line_eval in reader:
+			sequence = Sequence(line_eval[2], line_eval[0])
 			sequences.append(sequence)
-			point_datas[0].add_data("row", [sequence.get_name()], ["".join(sequence.get_sequence()), line_val[1]])
+			point_datas[0].add_data("row", [sequence.get_name()], ["".join(sequence.get_sequence()), line_eval[1]])
 
 	point_datas[0].set_label("column", ["Sequence", "Exp."])
 	point_datas[0].set_dtype(np.float, "label", "Exp.")
 	point_datas[1] = copy.deepcopy(point_datas[0])
 	point_datas[2] = copy.deepcopy(point_datas[0])
+
 
 	# optimize parameter
 	energy_idx = 2
@@ -71,7 +72,8 @@ if __name__ == '__main__':
 		direction = [0 for x in parameter_types]
 		flag_first = True
 
-		r_eval = []
+		e_eval = []
+		e_prev = []
 		while args.threshold_increment < increment:
 			# loop while increment is larger than threshold
 
@@ -82,7 +84,7 @@ if __name__ == '__main__':
 
 				if flag_first:
 					point_data.add_data("column", [parameter_type], [sequence.set_parameter(candidate_parameters[parameter_idx]).get_energy() for sequence in sequences], np.float)
-					r_eval = [0.0 for x in parameter_types]
+					e_eval = [0.0 for x in parameter_types]
 
 				if flag_change[parameter_idx]:
 					# for parameter changed with current increment
@@ -90,31 +92,34 @@ if __name__ == '__main__':
 					# change parameter
 					if direction[parameter_idx] == False:
 						# not determine direction
+
+						# create +/- increment parameter object
 						parameter_plus = Parameter(parameter_type + "_plus")
-						parameter_plus.set_parameter("all", {k: v + increment if k == candidate_parameters[parameter_idx].get_name() else v for k, v in candidate_parameters[parameter_idx].get_parameter().items()})
+						parameter_plus.set_parameter("all", candidate_parameters[parameter_idx].get_parameter())
+						parameter_plus.set_parameter(parameter_type, parameter_plus.get_parameter(parameter_type) + increment)
 						parameter_minus = Parameter(parameter_type + "_minus")
-						parameter_minus.set_parameter("all", {k: v - increment if k == candidate_parameters[parameter_idx].get_name() else v for k, v in candidate_parameters[parameter_idx].get_parameter().items()})
+						parameter_minus.set_parameter("all", candidate_parameters[parameter_idx].get_parameter())
+						parameter_minus.set_parameter(parameter_type, parameter_minus.get_parameter(parameter_type) - increment)
 
 						# add energy
 						point_data.add_data("column", [parameter_type + "_plus"], [sequence.set_parameter(parameter_plus).get_energy() for sequence in sequences], np.float)
 						point_data.add_data("column", [parameter_type + "_minus"], [sequence.set_parameter(parameter_minus).get_energy() for sequence in sequences], np.float)
 
-						# calculate r2 & determine direction
-						r = []
-						r.append(point_data.get_factor("label", "Exp.", parameter_type)[2])
-						r.append(point_data.get_factor("label", "Exp.", parameter_type + "_plus")[2])
-						r.append(point_data.get_factor("label", "Exp.", parameter_type + "_minus")[2])
-						r = [0.0 if np.isnan(x) else x for x in r]
-						max_idx = [i for i, x in enumerate(r) if max(r) == x]
-						if len(max_idx) != 1 or max_idx[0] == 0:
-							# When all r is the same even if parameter is changed
-							# When r value for base parameter is closest to 1, lock changing
+						# calculate error
+						e = [point_data.get_diff("label", "Exp.", parameter_type)[1], point_data.get_diff("label", "Exp.", parameter_type + "_plus")[1], point_data.get_diff("label", "Exp.", parameter_type + "_minus")[1]]
+						print(100,e)
+						e_prev = e
+						min_e = min(e)
+						min_e_idx = [i for i, x in enumerate(e) if min_e == x]
+						if len(min_e_idx) != 1 or min_e_idx[0] == 0:
+							# When all e is the same even if parameter is changed
+							# When e value for base parameter is closest to 1, lock changing
 							flag_change[parameter_idx] = False
-							r_eval[parameter_idx] = r[0]
+							e_eval[parameter_idx] = e[0]
 							point_data.remove_data("column", "label", parameter_type + "_plus")
 							point_data.remove_data("column", "label", parameter_type + "_minus")
 
-						elif max_idx[0] == 1:
+						elif min_e_idx[0] == 1:
 							# When r value for high parameter is closest to 1, update base parameter to high parameter
 							parameter_plus.set_name(parameter_type)
 							candidate_parameters[parameter_idx] = parameter_plus
@@ -122,9 +127,10 @@ if __name__ == '__main__':
 							point_data.remove_data("column", "label", parameter_type)
 							point_data.rename_label("column", parameter_type + "_plus", parameter_type)
 							direction[parameter_idx] = 1
-							r_eval[parameter_idx] = r[1]
+							e_eval[parameter_idx] = e[1]
+							print("shift plus")
 
-						elif max_idx[0] == 2:
+						elif min_e_idx[0] == 2:
 							# When r value for low parameter is closest to 1, update base parameter to low parameter
 							parameter_minus.set_name(parameter_type)
 							candidate_parameters[parameter_idx] = parameter_minus
@@ -132,7 +138,8 @@ if __name__ == '__main__':
 							point_data.remove_data("column", "label", parameter_type)
 							point_data.rename_label("column", parameter_type + "_minus", parameter_type)
 							direction[parameter_idx] = -1
-							r_eval[parameter_idx] = r[2]
+							e_eval[parameter_idx] = e[2]
+							print("shift minus")
 
 						else:
 							sys.stderr.write("ERROR: undefined condition.\n")
@@ -147,52 +154,59 @@ if __name__ == '__main__':
 						# add energy
 						point_data.add_data("column", [parameter_type + "_new"], [sequence.set_parameter(parameter_new).get_energy() for sequence in sequences], np.float)
 
-						# calculate r2 & determine direction
-						r = []
-						r.append(point_data.get_factor("label", "Exp.", parameter_type)[2])
-						r.append(point_data.get_factor("label", "Exp.", parameter_type + "_new")[2])
-						r = [0.0 if np.isnan(x) else x for x in r]
-						max_idx = [i for i, x in enumerate(r) if max(r) == x]
-						if len(max_idx) != 1 or max_idx[0] == 0:
+						# calculate error
+						e = [point_data.get_diff("label", "Exp.", parameter_type)[1], point_data.get_diff("label", "Exp.", parameter_type + "_new")[1]]
+						print(101, e)
+						e_prev = e
+						min_e = min(e)
+						min_e_idx = [i for i, x in enumerate(e) if min_e == x]
+						if len(min_e_idx) != 1 or min_e_idx[0] == 0:
 							# When r value for base parameter is closest to 1, change increment
 							# When r value for prev parameter is closest to 1, lock changing
 							flag_change[parameter_idx] = False
 							point_data.remove_data("column", "label", parameter_type + "_new")
+							print("fail")
 
-						elif max_idx[0] == 1:
+						elif min_e_idx[0] == 1:
 							# When r value for new parameter is closest to 1, update base parameter to low parameter
+							print("old", parameter.get_parameter())
 							parameter_new.set_name(parameter_type)
 							candidate_parameters[parameter_idx] = parameter_new
 							point_data.remove_data("column", "label", parameter_type)
 							point_data.rename_label("column", parameter_type + "_new", parameter_type)
-							r_eval[parameter_idx] = r[1]
+							e_eval[parameter_idx] = e[1]
+							print("new", parameter_new.get_parameter())
+							print("update")
 
 						else:
 							sys.stderr.write("ERROR: undefined condition.\n")
 							sys.exit(1)
+				# print(point_data.get_data())
+				# print(e_prev, e)
+				# print(parameter_type)
+				sys.stdin.readline()
 
-			# evaluation for r
-			max_r = max(r_eval)
-			max_idx = [i for i, v in enumerate(r_eval) if v == max_r][0]
+			# evaluation for error
+			print(e_prev, e)
+			min_e = max(e_eval)
+			min_e_idx = [i for i, v in enumerate(e_eval) if v == min_e][0]
 
 			# update_parameter
-			new_parameter_type = candidate_parameters[max_idx].get_name()
-			new_parameter_val = candidate_parameters[max_idx].get_parameter(new_parameter_type)
+			new_parameter_type = candidate_parameters[min_e_idx].get_name()
+			new_parameter_val = candidate_parameters[min_e_idx].get_parameter(new_parameter_type)
 			for parameter in candidate_parameters:
-				parameter.update_parameter(new_parameter_type, new_parameter_val)
-			parameters[energy_idx + 3] = candidate_parameters[max_idx]
+				parameter.set_parameter(new_parameter_type, new_parameter_val)
+			parameters[energy_idx + 3] = candidate_parameters[min_e_idx]
 			flag_first = False
 
 			print(parameters[energy_idx + 3].get_parameter())
-			print("r", r_eval[max_idx])
-			for i, (p, r) in enumerate(zip(parameter_types, r_eval)):
-				if i == max_idx:
+			print("e", e_eval[min_e_idx])
+			for i, (p, r) in enumerate(zip(parameter_types, e_eval)):
+				if i == min_e_idx:
 					print(p, r, "O")
 				else:
 					print(p, r)
 
-			print("change", flag_change)
-			print("direction", direction)
 			if flag_change.count(False) == len(parameter_types):
 				# When all parameters were locked, unlock and change increment
 				increment /= 2
@@ -200,7 +214,6 @@ if __name__ == '__main__':
 				print("next_step", increment)
 				print("=" * 50)
 				sys.stdin.readline()
-			print("=" * 50)
 
 	point_datas[2].save_csv("point.csv")
 
