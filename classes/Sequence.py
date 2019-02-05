a359b39f3f215944b28ce56c6d1670003d63b30a
mumeiyamibito <mumeiyamibito@gmail.com>
2019-02-05 19:11:54 +0900

    fix: remove complement list and change searching compliment pair type in Sequence class.

--- classes/Sequence.py
+++ classes/Sequence.py
@@ -9,18 +9,6 @@ from classes.Parameter import Parameter
 # =============== variable =============== #
 BASE_PAIR = {"A": "T", "T": "A", "G": "C", "C": "G"}
 parameter_list = ["AA/TT", "AT/TA", "TA/AT", "CA/GT", "GT/CA", "CT/GA", "GA/CT", "CG/GC", "GC/CG", "GG/CC", "init_GC", "init_AT", "symmetry", "5term_TA"]
-complement_list = {
-	"AA/TT": "TT/AA",
-	"AT/TA": "AT/TA",
-	"TA/AT": "TA/AT",
-	"CA/GT": "TG/AC",
-	"GT/CA": "AC/TG",
-	"CT/GA": "AG/TC",
-	"GA/CT": "TC/AG",
-	"CG/GC": "CG/GC",
-	"GC/CG": "GC/CG",
-	"GG/CC": "CC/GG"
-}
 
 
 # =============== class =============== #
@@ -164,12 +152,15 @@ class Sequence:
 			if self._sequence[0] == "T":
 				freq["5term_TA"] += 1
 
+		if self._is_self_complement:
+			freq["symmetry"] += 1
+
 		for base_idx in range(len(self._sequence) - 1):
-			pair_forward = self._sequence[base_idx] + self._sequence[base_idx + 1]
-			pair_reverse = BASE_PAIR[self._sequence[base_idx]] + BASE_PAIR[self._sequence[base_idx + 1]]
-			pair_type = "/".join([pair_forward, pair_reverse])
-			if pair_type not in freq.keys():
-				pair_type = [k for k, v in complement_list.items() if v == pair_type][0]
+			pair_forward = [self._sequence[base_idx], self._sequence[base_idx + 1]]
+			pair_reverse = [BASE_PAIR[self._sequence[base_idx]], BASE_PAIR[self._sequence[base_idx + 1]]]
+			pair_type = "/".join(["".join(pair_forward), "".join(pair_reverse)])
+			if pair_type not in parameter_list:
+				pair_type = "/".join(["".join(reversed(pair_reverse)), "".join(reversed(pair_forward))])
 			freq[pair_type] += 1
 
 		return freq
@@ -198,9 +189,12 @@ class Sequence:
 
 
 		for base_idx in range(len(self._sequence) - 1):
-			pair_forward = self._sequence[base_idx] + self._sequence[base_idx + 1]
-			pair_reverse = BASE_PAIR[self._sequence[base_idx]] + BASE_PAIR[self._sequence[base_idx + 1]]
-			pair_type = "/".join([pair_forward, pair_reverse])
+			pair_forward = [self._sequence[base_idx], self._sequence[base_idx + 1]]
+			pair_reverse = [BASE_PAIR[self._sequence[base_idx]], BASE_PAIR[self._sequence[base_idx + 1]]]
+			pair_type = "/".join(["".join(pair_forward), "".join(pair_reverse)])
+			if pair_type not in parameter_list:
+				pair_type = "/".join(["".join(reversed(pair_reverse)), "".join(reversed(pair_forward))])
+
 			energy += obj_parameter.get_parameter(pair_type)
 
 		if self._is_self_complement:
