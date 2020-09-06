105c71f7c700439a8d16654e6910ba59adb7c793
Tatsuya Ohyama <t.ohyama.0109.02442@gmail.com>
2020-09-07 00:23:27 +0900

    feat: add one-leave-out_input_generator.py

---
+++ one-leave-out_input_generator.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+one-leave-out_input_generator.py
+Program to generate input file for one leave out method
+"""
+
+import sys, signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+import argparse
+import os
+import csv
+import copy
+
+from basic_func import check_exist, check_overwrite
+
+
+# =============== main =============== #
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description = "Program to generate input file for one leave out method", formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument("-i", dest = "INPUT_FILE", metavar = "INPUT.csv", required = True, help = "original input file for NNfitTool.py")
+	parser.add_argument("-O", dest = "FLAG_OVERWRITE", action = "store_true", default = False, help = "overwrite forcibly")
+	args = parser.parse_args()
+
+	check_exist(args.INPUT_FILE, 2)
+
+	label = []
+	values = []
+	with open(args.INPUT_FILE, "r") as obj_input:
+		reader = csv.reader(obj_input)
+		for row_idx, row_val in enumerate(reader, 1):
+			if row_idx == 1:
+				label.extend(row_val)
+			else:
+				tmp_values = row_val[:2] + [float(v) for v in row_val[2:] if len(v) != 0]
+				values.append(tmp_values)
+
+	digit = len(str(len(values)))
+	output_prefix = os.path.splitext(args.INPUT_FILE)[0]
+	output_format = output_prefix + "_wo_{0:0" + str(digit) + "}.csv"
+	for i in range(len(values[1:])):
+		output_file = output_format.format(i + 1)
+
+		if args.FLAG_OVERWRITE == False:
+			check_overwrite(output_file)
+
+		with open(output_file, "w") as obj_output:
+			writer = csv.writer(obj_output)
+			writer.writerow(label)
+			tmp_values = copy.deepcopy(values)
+			del(tmp_values[i])
+			writer.writerows(tmp_values)
+
+		sys.stderr.write("Create: {0}\n".format(output_file))
