14b1a5c922bb3b8a8231b8ebfa5313d0d65cf986
Tatsuya Ohyama <t.ohyama.0109.02442@gmail.com>
2022-09-27 10:23:28 +0900

    style: change style based on PEP8

--- NNcalcTool.py
+++ NNcalcTool.py
@@ -13,35 +13,62 @@ import csv
 import os
 import datetime
 
-from basic_func import check_exist, check_overwrite
-from classes.Parameter import Parameter
-from classes.Sequence import Sequence
+from classes.basicfunc import check_exist, check_overwrite
+from classes.parameter import Parameter
+from classes.sequence import Sequence
+
 
 
 # =============== variable =============== #
 VERSION = "1.0"
+TEMPLATE_PARAM = "template_ref_param.csv"
+TEMPLATE_SEQUENCE = "template_sequence.csv"
+
+
 
 # =============== function =============== #
+def make_template(flag_overwrite):
+	"""
+	create template files for ref_param.csv and ref_exp.csv
+	"""
+	if flag_overwrite == False:
+		check_overwrite(TEMPLATE_PARAM)
+	with open(TEMPLATE_PARAM, "w") as obj_output:
+		writer = csv.writer(obj_output)
+		writer.writerow(["Parameter", "dH", "dS", "dG"])
+	sys.stderr.write("{0} is created.\n".format(TEMPLATE_PARAM))
+
+	if flag_overwrite == False:
+		check_overwrite(TEMPLATE_SEQUENCE)
+	with open(TEMPLATE_SEQUENCE, "w") as obj_output:
+		writer = csv.writer(obj_output)
+		writer.writerow(["Label", "Sequence"])
+	sys.stderr.write("{0} is created.\n".format(TEMPLATE_SEQUENCE))
 
 
 
 # =============== main =============== #
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description = "neighbor method", formatter_class=argparse.RawTextHelpFormatter)
-	parser.add_argument("-s", dest = "file_sequence", metavar = "SEQUENCE_FILE.csv", help = "sequence file of csv format")
-	parser.add_argument("-p", dest = "file_parameter", metavar = "PARAMETER_FILE.csv", required = True, help = "input file for parameters")
-	parser.add_argument("-o", dest = "file_output", metavar = "OUTPUT_FILE.csv", required = True, help = "output file")
-	parser.add_argument("-l", dest = "file_log", metavar = "LOG_FILE.log", help = "log file (if not specify, a log file with the same name as -o option is generated)")
-	parser.add_argument("-O", dest = "flag_overwrite", action = "store_true", default = False, help = "overwrite forcibly")
+	parser = argparse.ArgumentParser(description="neighbor method", formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument("-s", dest="FILE_SEQUENCE", metavar="SEQUENCE_FILE.csv", required="--make-template" not in sys.argv, help="sequence file of csv format")
+	parser.add_argument("-p", dest="FILE_PARAMETER", metavar="PARAMETER_FILE.csv", required="--make-template" not in sys.argv, help="input file for parameters")
+	parser.add_argument("-o", dest="FILE_OUTPUT", metavar="OUTPUT_FILE.csv", required="--make-template" not in sys.argv, help="output file")
+	parser.add_argument("-l", dest="FILE_LOG", metavar="LOG_FILE.log", help="log file (if not specify, a log file with the same name as -o option is generated)")
+	parser.add_argument("-O", dest="FLAG_OVERWRITE", action="store_true", default=False, help="overwrite forcibly")
+	parser.add_argument("--make-template", dest="FLAG_MAKE_TEMPLATE", action="store_true", default=False, help="make template files ({0} and {1}) and exit".format(TEMPLATE_PARAM, TEMPLATE_SEQUENCE))
 	args = parser.parse_args()
 
-	check_exist(args.file_parameter, 2)
-	check_exist(args.file_sequence, 2)
+	if args.FLAG_MAKE_TEMPLATE:
+		make_template(args.FLAG_OVERWRITE)
+		sys.exit(0)
+
+	check_exist(args.FILE_PARAMETER, 2)
+	check_exist(args.FILE_SEQUENCE, 2)
 
 	# reading parameters
 	parameters = []
 	base_pair = {}
-	with open(args.file_parameter, "r") as obj_input:
+	with open(args.FILE_PARAMETER, "r") as obj_input:
 		flag_read = False
 		flag_init = False
 		reader = csv.reader(obj_input)
@@ -55,16 +82,17 @@ if __name__ == '__main__':
 				break
 
 			if flag_read:
-				if "/" in line_val[0] and not line_val[0].startswith("init") and not line_val[0].startswith("length") and not line_val[0].startswith("symmetry") and not line_val[0].startswith("re:"):
+				if "/" in line_val[0] and not line_val[0].startswith("init") and not line_val[0].startswith("length") and not line_val[0].startswith("symmetry") and not line_val[0].startswith("re:") and not line_val[0].startswith("reg:"):
 					# generate base_pair
 					base = line_val[0].split("/", 2)
 					tmp_base_pair = {}
-					tmp_base_pair[base[0][0:1]]= base[1][0:1]
-					tmp_base_pair[base[0][1:2]]= base[1][1:2]
+					tmp_base_pair[base[0][0:1]] = base[1][0:1]
+					tmp_base_pair[base[0][1:2]] = base[1][1:2]
 					for k, v in tmp_base_pair.items():
 						if k in base_pair.keys():
 							if base_pair[k] != v:
-								sys.stderr.write("ERROR: base pair are duplicated: {0}-{1} vs {0}-{2}.\n".format(k, base_pair[k], tmp_base_pair[k]))
+								sys.stderr.write("ERROR: base pair is duplicated: {0}-{1} vs {0}-{2}.\n".format(k, base_pair[k], tmp_base_pair[k]))
+								sys.exit(1)
 						else:
 							base_pair[k] = v
 
@@ -81,7 +109,7 @@ if __name__ == '__main__':
 
 	# reading sequence
 	sequences = []
-	with open(args.file_sequence, "r") as obj_input:
+	with open(args.FILE_SEQUENCE, "r") as obj_input:
 		reader = csv.reader(obj_input)
 		flag_read = False
 		for line_val in reader:
@@ -93,33 +121,33 @@ if __name__ == '__main__':
 				sequences.append(Sequence(line_val[0]).set_sequence(line_val[1], base_pair))
 
 	# output
-	if args.file_log is not None:
-		path = os.path.splitext(args.file_output)
-		args.file_log = path[0] + ".log"
+	if args.FILE_LOG is not None:
+		path = os.path.splitext(args.FILE_OUTPUT)
+		args.FILE_LOG = path[0] + ".log"
 
-		if args.flag_overwrite == False:
-			check_overwrite(args.file_output)
-			check_overwrite(args.file_log)
+		if args.FLAG_OVERWRITE == False:
+			check_overwrite(args.FILE_OUTPUT)
+			check_overwrite(args.FILE_LOG)
 
-		with open(args.file_log, "w") as obj_output:
+		with open(args.FILE_LOG, "w") as obj_output:
 			obj_output.write("This log file was generated at " + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + "\n\n")
 			obj_output.write("<< Input >>\n")
 			obj_output.write("Program version: {0}\n".format(VERSION))
-			obj_output.write("Sequence file: {0} ({1} sequences)\n".format(args.file_sequence, len(sequences)))
-			obj_output.write("Parameter file: {0}\n".format(args.file_parameter))
+			obj_output.write("Sequence file: {0} ({1} sequences)\n".format(args.FILE_SEQUENCE, len(sequences)))
+			obj_output.write("Parameter file: {0}\n".format(args.FILE_PARAMETER))
 			obj_output.write("|{0:^15}|{1[0]:^8}|{1[1]:^8}|{1[2]:^8}|\n".format("", [param.get_name() for param in parameters]))
 			obj_output.write("|{0:-^15}|{0:-^8}|{0:-^8}|{0:-^8}|\n".format(""))
-			for parameter_type in parameters[0].get_parameter(data_type = "name"):
+			for parameter_type in parameters[0].get_parameter(data_type="name"):
 				obj_output.write("|{0:^15}|{1[0]:>8.3f}|{1[1]:>8.3f}|{1[2]:>8.3f}|\n".format(parameter_type, [param.get_parameter(parameter_type)[0] for param in parameters]))
 			obj_output.write("\n")
-			obj_output.write("Output file: {0}\n".format(args.file_output))
+			obj_output.write("Output file: {0}\n".format(args.FILE_OUTPUT))
 			obj_output.write("\n")
 
-	with open(args.file_output, "w") as obj_output:
+	with open(args.FILE_OUTPUT, "w") as obj_output:
 		writer = csv.writer(obj_output)
-		parameter_types = list(parameters[0].get_parameter(data_type = "name"))
-		writer.writerow(["Comment", "Sequence"] + [param.get_name() for param in parameters] + [""] + parameter_types)
+		parameter_types = list(parameters[0].get_parameter(data_type="name"))
+		writer.writerow(["Comment", "Sequence"] + [param.name for param in parameters] + [""] + parameter_types)
 		for sequence in sequences:
 			freq = sequence.get_freq(parameters[0], base_pair)
 			energy = [sequence.get_energy(param, base_pair) for param in parameters]
-			writer.writerow([sequence.get_name(), sequence.get_sequence("string")] + energy + [""] + [freq[param] for param in parameter_types])
+			writer.writerow([sequence.name, sequence.get_sequence("string")] + energy + [""] + [freq[param] for param in parameter_types])
---
+++ classes/datagroup.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import numpy as np
+import statistics
+
+
+# =============== class =============== #
+class DataGroup:
+	""" DataGroup class """
+	def __init__(self, name):
+		"""
+		@param name: this object name
+		"""
+		# member variables
+		self._name = ""
+		self._sequences = []
+		self._energy = []
+		self._error = []
+		self._error_sign = []
+		self._base_pairs = {}
+		self._is_fitting = None
+
+		self.set_name(name)
+
+
+	@property
+	def name(self):
+		return self._name
+
+	@property
+	def sequences(self):
+		return self._sequences
+
+	@property
+	def energy(self):
+		return self._energy
+
+	@property
+	def error(self):
+		return self._error
+
+	@property
+	def error_sign(self):
+		return self._error_sign
+
+	@property
+	def base_pairs(self):
+		return self._base_pairs
+
+	@property
+	def is_fitting(self):
+		return self._is_fitting
+
+
+	def set_name(self, name):
+		"""
+		Method to set name
+
+		Args:
+			name (str): data name
+
+		Returns:
+			self
+		"""
+		self._name = name
+		return self
+
+
+	def set_base_pair(self, base_pairs):
+		"""
+		Method to set base pair information
+
+		Args:
+			base_pairs (dict): base pairs
+
+		Returns:
+			self
+		"""
+		self._base_pairs = base_pairs
+		return self
+
+
+	def append(self, obj_sequence, exp_value, exp_value_e=0.0):
+		"""
+		Method to append sequence object and experimental data
+
+		Args:
+			obj_sequence (objSequence): Sequence Object
+			exp_value (float): experimental value
+			exp_value_e (float, optional): error value of experimental value (Default: 0.0)
+
+		Returns:
+			self
+		"""
+		self._sequences.append(obj_sequence)
+		self._energy.append(exp_value)
+		self._error.append(exp_value_e)
+		self._error_sign.append(0)
+		if len([v for v in self._energy if v == 0.0]) == len(self._energy):
+			# If all the values are 0.0, it can not be fitted and will not be calculated later
+			self._is_fitting = False
+		else:
+			self._is_fitting = True
+
+		return self
+
+
+	def get_sequence(self, data_type=None):
+		"""
+		Method to return Sequence object
+
+		Args:
+			data_type (None or str, optional): None or "sequence" (Default: None)
+
+		Returns:
+			list of objSequence for None or sequence string list for "sequence"
+		"""
+		if data_type is None:
+			return self._sequences
+		elif data_type == "sequence":
+			return [x.get_sequence() for x in self._sequences]
+
+
+	def get_energy(self, flag_sequence=False, obj_parameters=[]):
+		"""
+		Method to return experimental value list
+
+		Args:
+			flag_sequence (bool, optional): return energy with sequence (Default: False)
+			obj_parameters (list, optional): Paramete robject list (Default: [])
+
+		Returns:
+			list: energy value
+		"""
+		energy_data = []
+		for idx in range(len(self._sequences)):
+			data = []
+			if flag_sequence:
+				data.append(self._sequences[idx].get_sequence("string"))
+			data.append(self._energy[idx])
+			data.append(self._error[idx])
+			for parameter in obj_parameters:
+				data.append(self._sequences[idx].get_energy(parameter, self._base_pairs))
+			energy_data.append(data)
+		return energy_data
+
+
+	def get_stat(self, obj_parameter, mode=None, deg=1, error_sign=None):
+		"""
+		Method to return statistics
+
+		Args:
+			obj_parameter (str): parameter name
+			mode (None or str, optional): None, "r", "r2", "slope", "intercept", "diff_abs", "diff_mean", "diff_std", "diff_sum, diff_square" (Default: None)
+			deg (int, optional): dimension for curve fitting (Default: 1)
+			error_sign (list, optional): sign for error (Default: None)
+
+		Returns:
+			list: [r, r2, slope, intercept, diff_abs, diff_mean, diff_std, diff_sum, diff_square] list when data_type is None
+		"""
+		if self._is_fitting:
+			if type(error_sign) != list:
+				error_sign = self._error_sign
+			x = np.array([self._energy[idx] + self._error[idx] * error_sign[idx] for idx in range(len(self._energy))])
+			y = np.array([sequence.get_energy(obj_parameter, self._base_pairs) for sequence in self._sequences])
+			result = [float(x) for x in np.polyfit(x, y, deg).tolist()]
+			if y[y == 0.0].shape[0] == len(self._sequences) or np.std(x) == 0.0 or np.std(y) == 0.0:
+				result.append(0.0)
+			else:
+				result.append(np.corrcoef(x, y)[0, 1])
+
+			result.append(result[-1] ** 2)
+			result.append(np.abs(x - y))
+			result.append(np.mean(x - y))
+			result.append(np.std(x - y))
+			result.append(np.sum(np.abs(x - y)))
+			result.append(np.sum((x - y) ** 2))
+
+		else:
+			result = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+		if mode is None:
+			return result
+		elif mode == "slope":
+			return result[0]
+		elif mode == "intercept":
+			return result[1]
+		elif mode == "r":
+			return result[2]
+		elif mode == "r2":
+			return result[3]
+		elif mode == "diff_abs":
+			return result[4]
+		elif mode == "diff_mean":
+			return result[5]
+		elif mode == "diff_std":
+			return result[6]
+		elif mode == "diff_sum":
+			return result[7]
+		elif mode == "diff_square":
+			return result[8]
+		else:
+			sys.stderr.write("ERROR: undefined mode at get_stat() in DataGroup class.\n")
+			sys.exit(1)
